ef861052ed7a38646478a098a24084a53c6ba370
Evan Lee <evan.lee.615@gmail.com>
2024-02-27 10:02:07 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    let text = document.getElementById('new-task-description').value;
+    
+    let list = document.createElement('li');
+    list.textContent = text
+    document.getElementById('tasks').appendChild(list);
+  })
 });
